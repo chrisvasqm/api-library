dd762ac1f98f8d07617b7262ee4e729db49e1f5b
chrisvasqm <christianvasquezmelo@gmail.com>
2024-04-09 15:39:31 -0400

    Add get authors API

--- src/routes/authors.ts
+++ src/routes/authors.ts
@@ -1,9 +1,11 @@
 import { Router, Request, Response } from 'express';
+import prisma from '../../prisma/client'
 
 const router = Router();
 
-router.get('/', (_: Request, response: Response) => {
-    response.send('Authors data');
+router.get('/', async (_: Request, response: Response) => {
+    const authors = await prisma.author.findMany();
+    response.send(authors);
 });
 
 export default router;
