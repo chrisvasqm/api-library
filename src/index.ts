6c2cf7ac9a57d5440fe66c7d23a5275bd49c0315
chrisvasqm <christianvasquezmelo@gmail.com>
2024-04-09 17:51:10 -0400

    Add home route

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import cors from 'cors';
-import express from 'express';
+import express, { Request, Response } from 'express';
 import helmet from 'helmet';
 import authorsRouter from './routes/authors';
 import booksRouter from './routes/books';
@@ -9,6 +9,10 @@ const app = express();
 app.use(cors());
 app.use(helmet())
 app.use(express.json());
+
+app.use('/api', (_: Request, response: Response) => {
+    response.send('Welcome to the Book Store API.')
+});
 
 app.use('/api/authors', authorsRouter);
 app.use('/api/books', booksRouter);
