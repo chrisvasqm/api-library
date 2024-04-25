6d1cceac9b6b9a54dd9f6e24f1ac210e7c6114b4
chrisvasqm <christianvasquezmelo@gmail.com>
2024-04-25 00:40:29 -0400

    Update welcome message

--- src/routes/home.ts
+++ src/routes/home.ts
@@ -3,7 +3,7 @@ import { Router, Request, Response } from 'express';
 const router = Router();
 
 router.get('/', (_: Request, response: Response) => {
-    response.send('Welcome to the Book Store API.')
+    response.send('Welcome to the Library API.')
 });
 
 export default router;
