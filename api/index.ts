eb3e2c557b98d3cd7def39c651740ff921412e87
chrisvasqm <christianvasquezmelo@gmail.com>
2024-05-12 18:24:37 -0400

    Reorganize files for Vercel

--- api/index.ts
+++ api/index.ts
@@ -1,5 +1,25 @@
-import app from '../src/app'
+import cors from 'cors';
+import express from 'express';
+import helmet from 'helmet';
+import homeRouter from './routes/home';
+import authorsRouter from './routes/authors';
+import booksRouter from './routes/books';
+import usersRouter from './routes/users';
 
-// This module is necessary for Vercel deployments
+const app = express();
+
+app.use(cors());
+app.use(helmet());
+app.use(express.json());
+
+app.use('/', homeRouter);
+app.use('/api/authors', authorsRouter);
+app.use('/api/books', booksRouter);
+app.use('/api/users', usersRouter);
+
+const PORT = 8080;
+app.listen(PORT || 3030, () => {
+    console.log(`Library API is running on http://localhost:${PORT}`);
+});
 
 export default app;
