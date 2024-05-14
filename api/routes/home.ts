841d0e4bd2eec8e1257bf7812fafa2ce01f68fae
chrisvasqm <christianvasquezmelo@gmail.com>
2024-05-14 07:58:36 -0400

    Impove home route presentation

--- api/routes/home.ts
+++ api/routes/home.ts
@@ -1,9 +1,37 @@
-import { Router, Request, Response } from 'express';
+import { Router } from 'express';
 
 const router = Router();
 
-router.get('/', (_: Request, response: Response) => {
-    response.send('Welcome to the Library API.')
-});
+router.get('/', (_, response) => {
+    response.setHeader('Content-Type', 'text/html');
+    response.send(html);
+})
+
+const html = `
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Welcome to Library API</title>
+        <style>
+            body {
+                font-family: Arial, sans-serif;
+                background-color: #f0f0f0;
+                padding: 20px;
+                text-align: center;
+            }
+            h1 {
+                color: #333;
+                margin-bottom: 20px;
+            }
+        </style>
+    </head>
+    <body>
+        <h1>Welcome to Library API</h1>
+        <p>Learn more about this API at <a href="https://www.postman.com/dark-sunset-399073/workspace/library-api/overview">here</a>.</p>
+    </body>
+</html>
+`;
 
 export default router;
